1d1f85840f51df19714b86aa999f70eab8e2877f
Andre Cruz <andr3.cruz96@gmail.com>
2023-05-05 00:28:22 +0100

    feature/ Client refactor

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,10 @@
 package client;
 
+import protocol.Message;
+import protocol.MessageType;
+
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
@@ -14,86 +16,81 @@ public class Client {
         int port = Integer.parseInt(args[1]);
 
         try (Socket socket = new Socket(hostname, port)) {
-
-            InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-
-            System.out.println("Bem Vindo!");
-
             Scanner scanner = new Scanner(System.in);
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            output.flush();
 
-            String token = getClientToken(); //Token do cliente
-            writer.println(token); //Envia token para o servidor
-            String validToken = reader.readLine(); //Resposta do servidor pra ver se o token é valido
+            System.out.println("WELCOME!");
 
-            if (validToken.equals("Token authorized")){
+            String token = getClientToken();
+
+            output.writeObject(new Message(MessageType.LOGIN_TOKEN, token));
+            output.flush();
+
+            Message message = (Message) input.readObject();
+            if (message.getType() == MessageType.AUTHENTICATED_TOKEN) {
                 System.out.println("Authentication succeeded.");
-            }
+            } else {
+                System.out.println(message.getMessage());
 
-            else{
                 boolean authenticated = false;
-                do {
+                while (!authenticated) {
+
                     System.out.print("\nDo you want to register or login? (r/l) ");
                     String option = scanner.nextLine();
-                    if (option.equals("r")) {
-                        writer.println("register");
 
-                        System.out.println("Register -----------");
-                        System.out.print("Username: ");
-                        String username = scanner.nextLine();
-                        writer.println(username);
+                    switch (option) {
+                        case "r" -> {
+                            System.out.println("Register -----------");
+                            System.out.print("Username: ");
+                            String username = scanner.nextLine();
 
-                        System.out.print("Password: ");
-                        String password = scanner.nextLine();
-                        writer.println(password);
+                            System.out.print("Password: ");
+                            String password = scanner.nextLine();
 
-                        String authResult = reader.readLine();
-                        System.out.println(authResult);
-                    } else if (option.equals("l")) {
-                        writer.println("login");
+                            output.writeObject(new Message(MessageType.REGISTER, token, username, password));
+                            output.flush();
 
-                        System.out.println("Login -----------");
-                        System.out.print("Username: ");
-                        String username = scanner.nextLine();
-                        writer.println(username);
+                            System.out.println(((Message) input.readObject()).getMessage());
+                        }
 
-                        System.out.print("Password: ");
-                        String password = scanner.nextLine();
-                        writer.println(password);
+                        case "l" -> {
+                            System.out.println("Login -----------");
+                            System.out.print("Username: ");
+                            String username = scanner.nextLine();
 
-                        String authResult = reader.readLine();
-                        System.out.println(authResult);
-                        authenticated = authResult.equals("Authentication succeeded.");
-                    } else {
-                        System.out.println("Invalid option. Please try again.");
+                            System.out.print("Password: ");
+                            String password = scanner.nextLine();
+
+                            output.writeObject(new Message(MessageType.LOGIN, token, username, password));
+                            output.flush();
+
+                            Message authResult = (Message) input.readObject();
+                            authenticated = authResult.getType() == (MessageType.AUTHENTICATED);
+                            System.out.println(authResult.getMessage());
+                            if (authenticated) {
+                                String newToken = authResult.getToken();
+                                updateToken(newToken);
+                            }
+                        }
+                        default -> System.out.println("Invalid option. Please try again.");
                     }
-
-                } while (!authenticated);
-
-                String newToken = reader.readLine();
-                updateToken(newToken);
+                }
             }
-        } catch (UnknownHostException ex) {
-
-            System.out.println("Server not found: " + ex.getMessage());
-
-        } catch (IOException ex) {
-
-            System.out.println("I/O error: " + ex.getMessage());
+        } catch (IOException | ClassNotFoundException e) {
+            //TODO: handle exceptions
         }
     }
 
-    public static String getClientToken() {
-        String Token = null;
+    private static String getClientToken() {
+        String token = null;
         File file = new File("src/client/token.txt");
         if (file.exists()) {
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line = br.readLine();
                 if (line != null) {
-                    Token = line;
+                    token = line;
                 } else {
                     System.out.println("O arquivo está vazio.");
                 }
@@ -104,10 +101,10 @@ public class Client {
         else {
             System.out.println("O arquivo de token não existe na pasta atual.");
         }
-        return Token;
+        return token;
     }
 
-    public static void updateToken(String newToken) {
+    private static void updateToken(String newToken) {
         try {
             File file = new File("src/client/token.txt");
             if (file.exists()) {

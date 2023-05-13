8451e4af7e12d54d6420da084b153ce93aaebda8
Andre Cruz <andr3.cruz96@gmail.com>
2023-05-13 16:32:58 +0100

    fix/ updating user state after game ended

--- src/server/game/Game.java
+++ src/server/game/Game.java
@@ -3,12 +3,14 @@ package server.game;
 import protocol.Message;
 import protocol.MessageType;
 import server.models.Player;
+import server.models.UserState;
 
 
 import java.io.IOException;
 import java.util.List;
 
 import static server.data.UsersData.updateEloByUsername;
+import static server.data.UsersData.updateStateByUsername;
 
 
 public class Game implements Runnable {
@@ -31,6 +33,9 @@ public class Game implements Runnable {
 
             updateEloByUsername(playerOne.getUser().getUsername(), playerOneElo + 50);
             updateEloByUsername(playerTwo.getUser().getUsername(), playerTwoElo - 50);
+
+            updateStateByUsername(playerOne.getUser().getUsername(), -1L, UserState.NONE);
+            updateStateByUsername(playerTwo.getUser().getUsername(), -1L, UserState.NONE);
 
             playerOne.getOutputStream().writeObject(new Message(MessageType.END_GAME, playerOne.getUser().getToken(), "Game ended. You won!"));
             playerOne.getOutputStream().flush();
